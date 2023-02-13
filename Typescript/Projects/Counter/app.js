a1e5f3e6f1e8e6be5fd361cc6ead243458830ccb
jesus95santana <jesus95santana@yahoo.com>
2023-02-13 11:11:24 -0500

    added counter project

---
+++ Typescript/Projects/Counter/app.js
@@ -0,0 +1,38 @@
+const decreaseButton = document.getElementsByClassName("decrease")[0];
+const resetButton = document.getElementsByClassName("reset")[0];
+const increaseButton = document.getElementsByClassName("increase")[0];
+const value = document.getElementById("value");
+let counter = 0;
+let textColor = "black";
+function colorChange() {
+    if (counter < 0) {
+        textColor = "red";
+    }
+    else if (counter > 0) {
+        textColor = "blue";
+    }
+    else {
+        textColor = "black";
+    }
+}
+function decrease() {
+    counter -= 1;
+    colorChange();
+    value.style.color = textColor;
+    value.innerHTML = counter.toString();
+}
+function increase() {
+    counter += 1;
+    colorChange();
+    value.style.color = textColor;
+    value.innerHTML = counter.toString();
+}
+function reset() {
+    counter = 0;
+    colorChange();
+    value.style.color = textColor;
+    value.innerHTML = counter.toString();
+}
+decreaseButton.addEventListener("click", decrease);
+resetButton.addEventListener("click", reset);
+increaseButton.addEventListener("click", increase);
